da6e2282a2c42f13f3790ce78be4e32ecb1f2084
Danka <danielkarosas@gmail.com>
2025-10-12 17:05:16 +0100

    raspberrypi version new branch

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def start_cloudflare_tunnel():
         "cloudflared",
         "tunnel",
         "--config",
-        "C:\\Users\\boxatron\\.cloudflared\\config.yml",
+        "/home/boxraspberry/simpleInventory/.cloudflared/config.yml",
         "run",
         "mytunnel"
     ])
@@ -775,14 +775,14 @@ def finalize_barcodes():
         print(f"❌ Error finalizing barcodes: {e}")
 
 def start_flask():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8081)
 
 def start_tunnel():
     subprocess.Popen([
         "cloudflared",
         "tunnel",
         "--config",
-        "C:\\Users\\boxatron\\.cloudflared\\config.yml",
+        "/home/boxraspberry/simpleInventory/.cloudflared/config.yml",
         "run",
         "mytunnel"
     ])
