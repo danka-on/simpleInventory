cf89c50890fc975259e6785c085c235a8c3bcf80
danka-on <danielkarosas@gmail.com>
2025-11-05 09:10:28 -0500

    updates

--- _barcode_print_que_route.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-
-@app.route('/barcode-print-que')
-def barcode_print_que():
-    return render_template('barcode_print_que.html')
--- check_locations.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('searchRack.db')
-c = conn.cursor()
-c.execute('SELECT barcode, item_position, pictureposition FROM SEARCHRACK WHERE item_position IS NOT NULL OR pictureposition IS NOT NULL LIMIT 5')
-print('Items with location data:')
-for row in c.fetchall():
-    print(row)
-conn.close()
